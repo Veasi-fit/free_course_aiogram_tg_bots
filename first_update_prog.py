bb13f5a1f5083ea8011a267abf986842c3999543
Andrey Isaev <andrey.isaev.personal@gmail.com>
2024-02-29 18:42:53 +0300

    course_progress

---
+++ first_update_prog.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+API_URL = 'https://api.telegram.org/'
+BOT_TOKEN = '6831671836:AAFu3UcpcRJNfYKPy-usdYGKVS5NLE6g6gE'
+TEXT = 'Продолжай и скоро ты станешь миллионером!'
+MAX_COUNTER = 100
+
+
+offset = -2
+counter = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+            time.sleep(1)
+            counter += 1
